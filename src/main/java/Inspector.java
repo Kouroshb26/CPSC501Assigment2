a541333b405d5f33ee73bfa2498e5bb6f8c77911
Kourosh Banaeianzadeh <kouroshb.banaeianzade@ucalgary.ca>
2019-10-23 14:48:39 -0600

    Process arrays first
    Add pritln to space things out more often

--- src/main/java/Inspector.java
+++ src/main/java/Inspector.java
@@ -8,8 +8,8 @@ import java.util.stream.Collectors;
 
 public class Inspector {
 
-    public static void main(String[] args) {
-        new Inspector().inspect(new ClassB[12], true);
+    public static void main(String[] args) throws Exception {
+        new Inspector().inspect(new String[][]{{"hello", "helloWolrd"}}, true);
     }
 
     public void inspect(Object obj, boolean recursive) {
@@ -18,87 +18,89 @@ public class Inspector {
     }
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
-
-        //Name
-        println(depth, "The class name is " + c.getSimpleName());
-
-
-        //Supper Class
-        superClass(c, obj, recursive, depth);
-
-        //Interfaces
-        interfaceClasses(c, obj, recursive, depth);
-
-        //Constructors
-        if (c.getDeclaredConstructors().length > 1) {
-            println(depth, "The constructors are:");
-            for (Constructor constructor : c.getDeclaredConstructors()) {
-                println(depth, "===========================================");
-                println(depth, " The name is " + constructor.getName());
-                println(depth, " The parameters types are (" + Arrays.stream(constructor.getParameterTypes()).map(Class::getSimpleName).collect(Collectors.joining(",")) + ")");
-                println(depth, " The modifiers are " + Modifier.toString(constructor.getModifiers()));
-            }
-        }
-
-        //Declared Methods
-        if (c.getDeclaredMethods().length > 1) {
-            println(depth, "The methods are:");
-            for (Method method : c.getDeclaredMethods()) {
-                println(depth, "===========================================");
-                println(depth, " The name is " + method.getName());
-                println(depth, " The exceptions are (" + Arrays.stream(method.getExceptionTypes()).map(Class::getSimpleName).collect(Collectors.joining(",")) + ")");
-                println(depth, " The parameters types are (" + Arrays.stream(method.getParameterTypes()).map(Class::getSimpleName).collect(Collectors.joining(",")) + ")");
-                println(depth, " The return type is " + method.getReturnType().getSimpleName());
-                println(depth, " The modifiers are " + Modifier.toString(method.getModifiers()));
-            }
-        }
-
-        //Declared Fields
-        if (c.getDeclaredFields().length > 1) {
-            println(depth, "The fields are:");
-            for (Field field : c.getDeclaredFields()) {
-                field.setAccessible(true);
-                println(depth, "===========================================");
-                println(depth, " The name is " + field.getName());
-                println(depth, " The type is " + field.getType().getSimpleName());
-                println(depth, " The modifiers are " + Modifier.toString(field.getModifiers()));
-                try {
-                    if (obj != null) {
-                        Object fieldObject = field.get(obj);
-                        println(depth, " The value of the field is " + fieldObject);
-                        if (recursive) {
-                            inspectClass(field.getType(), fieldObject, recursive, depth + 1);
-                        } else {
-                            println(depth, variableHashCode(fieldObject, field.getType()));
-                        }
-                    }
-
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-
         if (c.isArray()) {
             println(depth, "This class is an array type with name " + c.getSimpleName());
             println(depth, "This is an array of " + c.getComponentType().getSimpleName());
             println(depth, "This array length is  " + Array.getLength(obj));
-            println(depth, "The contents of the array is :");
-            for (int i = 0; i < Array.getLength(obj); i++) {
-                if (recursive) {
+            println(depth, "The contents of the array is : " + objectToString(obj, c));
+            if (!c.getComponentType().isPrimitive() && recursive) {
+                for (int i = 0; i < Array.getLength(obj); i++) {
                     inspectClass(c.getComponentType(), Array.get(obj, i), recursive, depth + 1);
-                } else {
-                    println(depth, " " + variableHashCode(Array.get(obj, i), c.getComponentType()));
+                }
+            }
+        } else {
+            println(depth, "-----------------Start--------------------------");
+            //Name
+            println(depth, "The class name is " + c.getSimpleName());
+
+
+            //Supper Class
+            superClass(c, obj, recursive, depth);
+
+            //Interfaces
+            interfaceClasses(c, obj, recursive, depth);
+
+            //Constructors
+            if (c.getDeclaredConstructors().length > 0) {
+                System.out.println();
+                println(depth, "The constructors are:");
+                for (Constructor constructor : c.getDeclaredConstructors()) {
+                    println(depth, "===========================================");
+                    println(depth, " The name is " + constructor.getName());
+                    println(depth, " The parameters types are (" + Arrays.stream(constructor.getParameterTypes()).map(Class::getSimpleName).collect(Collectors.joining(",")) + ")");
+                    println(depth, " The modifiers are " + Modifier.toString(constructor.getModifiers()));
                 }
             }
 
+            //Declared Methods
+            if (c.getDeclaredMethods().length > 0) {
+                System.out.println();
+                println(depth, "The methods are:");
+                for (Method method : c.getDeclaredMethods()) {
+                    println(depth, "===========================================");
+                    println(depth, " The name is " + method.getName());
+                    println(depth, " The exceptions are (" + Arrays.stream(method.getExceptionTypes()).map(Class::getSimpleName).collect(Collectors.joining(",")) + ")");
+                    println(depth, " The parameters types are (" + Arrays.stream(method.getParameterTypes()).map(Class::getSimpleName).collect(Collectors.joining(",")) + ")");
+                    println(depth, " The return type is " + method.getReturnType().getSimpleName());
+                    println(depth, " The modifiers are " + Modifier.toString(method.getModifiers()));
+                }
+            }
+
+            //Declared Fields
+            if (c.getDeclaredFields().length > 0) {
+                System.out.println();
+                println(depth, "The fields are:");
+                for (Field field : c.getDeclaredFields()) {
+                    field.setAccessible(true);
+                    println(depth, "===========================================");
+                    println(depth, " The name is " + field.getName());
+                    println(depth, " The type is " + field.getType().getSimpleName());
+                    println(depth, " The modifiers are " + Modifier.toString(field.getModifiers()));
+                    try {
+                        if (obj != null) {
+                            Object fieldObject = field.get(obj);
+                            println(depth, " The value of the field is " + objectToString(fieldObject, field.getType()));
+                            if (recursive && !field.getType().isPrimitive()) {
+                                inspectClass(field.getType(), fieldObject, recursive, depth + 1);
+                            }
+                        } else {
+                            println(depth, "Object is null, thus no field value can be accessed");
+                        }
+
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
+        println(depth, "-----------------End--------------------------");
     }
+
 
     String interfaceClasses(Class c, Object obj, boolean recursive, int depth) {
         String result = null;
         if (c.getInterfaces().length > 0) {
+            System.out.println();
             result = " The interfaces are: " + Arrays.stream(c.getInterfaces()).map(Class::getSimpleName).collect(Collectors.joining(","));
             println(depth, result);
             Arrays.stream(c.getInterfaces()).forEach(inter -> inspectClass(inter, inter.cast(obj), recursive, depth + 1));
@@ -109,6 +111,7 @@ public class Inspector {
     String superClass(Class c, Object obj, boolean recursive, int depth) {
         String result = null;
         if (c.getSuperclass() != null) {
+            System.out.println();
             result = " The super class is " + c.getSuperclass().getSimpleName();
             println(depth, result);
             inspectClass(c.getSuperclass(), c.getSuperclass().cast(obj), recursive, depth + 1);
@@ -129,9 +132,24 @@ public class Inspector {
         System.out.println(getPrefixString(depth) + output);
     }
 
-    String variableHashCode(Object obj, Class<?> type) {
-        if (type.isPrimitive()) {
+    String objectToString(Object obj, Class<?> type) {
+        if (obj == null) {
+            return "null";
+        } else if (type.isPrimitive()) {
             return obj.toString();
+
+        } else if (type.isArray()) {
+            StringBuilder result = new StringBuilder();
+            result.append("[");
+            for (int i = 0; i < Array.getLength(obj); i++) {
+                result.append(objectToString(Array.get(obj, i), type.getComponentType()));
+                if (i + 1 != Array.getLength(obj)) {
+                    result.append(",");
+                }
+            }
+            result.append("]");
+            return result.toString();
+
         } else {
             return obj.getClass() + "@" + Integer.toHexString(System.identityHashCode(obj));
         }
