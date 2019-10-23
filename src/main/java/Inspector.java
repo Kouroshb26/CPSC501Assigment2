e7fd39becd5d5d768981d84f95476be3b323493e
Kourosh Banaeianzadeh <kouroshb.banaeianzade@ucalgary.ca>
2019-10-22 23:09:32 -0600

    Add Name and Super Class

--- src/main/java/Inspector.java
+++ src/main/java/Inspector.java
@@ -13,5 +13,22 @@ public class Inspector {
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
 
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            stringBuilder.append("\t");
+        }
+        String prefix = stringBuilder.toString();
+
+
+        //Name
+        System.out.println(prefix + "The class name is " + c.getSimpleName());
+
+
+        //Supper Class
+        if (c.getSuperclass() != null) {
+            System.out.println(prefix + " The super class is " + c.getSuperclass().getSimpleName());
+            inspectClass(c.getSuperclass(), c.getSuperclass().cast(obj), recursive, depth + 1);
+        }
+
     }
 }
